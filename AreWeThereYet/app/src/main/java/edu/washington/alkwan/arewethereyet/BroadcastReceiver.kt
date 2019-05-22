ebeea2b26dc93f59990a8f5f770e1a8c55bcf431
alkwan <kwan.alaina@gmail.com>
2019-05-21 19:17:39 -0700

    finished part 2!!!

--- AreWeThereYet/app/src/main/java/edu/washington/alkwan/arewethereyet/BroadcastReceiver.kt
+++ AreWeThereYet/app/src/main/java/edu/washington/alkwan/arewethereyet/BroadcastReceiver.kt
@@ -3,6 +3,7 @@ package edu.washington.alkwan.arewethereyet
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.telephony.SmsManager
 import android.util.Log
 import android.widget.Toast
 
@@ -11,9 +12,14 @@ class BroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val message = intent.getStringExtra("message")
         val number = intent.getStringExtra("number")
+        val smsManager = SmsManager.getDefault()
+        smsManager.sendTextMessage(
+            number,
+            null,
+            message,
+            null,
+            null
+        )
         Log.v("**BroadcastReceiver**", "Message: $message. Number: $number.")
-        val text = "Texting $number: $message"
-        val toast = Toast.makeText(context, text, Toast.LENGTH_LONG)
-        toast.show()
     }
 }
--- AreWeThereYet/app/src/main/java/edu/washington/alkwan/arewethereyet/MainActivity.kt
+++ AreWeThereYet/app/src/main/java/edu/washington/alkwan/arewethereyet/MainActivity.kt
@@ -1,11 +1,15 @@
 package edu.washington.alkwan.arewethereyet
 
+import android.Manifest
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
@@ -13,8 +17,16 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
+import android.widget.Toast.makeText
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val REQUEST_SMS_SEND_PERMISSION = 1234
+    }
+
+    private lateinit var alarmIntent: Intent
+    private lateinit var pendingIntent: PendingIntent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +38,7 @@ class MainActivity : AppCompatActivity() {
         val editMinutes = findViewById<EditText>(R.id.editMinutes)
 
         val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val intent = Intent(this, BroadcastReceiver::class.java)
+        alarmIntent = Intent(this, BroadcastReceiver::class.java)
         val regex = """(\([0-9]{3}\)\ [0-9]{3}\-[0-9]{4})""".toRegex()
 
         startButton.setOnClickListener {
@@ -41,37 +53,54 @@ class MainActivity : AppCompatActivity() {
                     val text = "Please make sure the phone number is in the (123) 456-7891 format."
                     val toast = Toast.makeText(this, text, Toast.LENGTH_LONG)
                     toast.show()
+                } else if (editMinutes.text.isEmpty()) {
+                    val text = "Please input a number."
+                    val toast = Toast.makeText(this, text, duration)
+                    toast.show()
                 } else if (editMinutes.text.toString().toInt() <= 0) {
                     val text = "Please input 1 minute or more."
                     val toast = Toast.makeText(this, text, duration)
                     toast.show()
                 } else {
+                    alarmIntent.putExtra("message", editMessage.text.toString())
+                    val areaCode = editPhoneNumber.text.toString().substring(1,4)
+                    val num1 = editPhoneNumber.text.toString().substring(6,9)
+                    val num2 = editPhoneNumber.text.toString().substring(10)
+                    val numTotal = areaCode + num1 + num2
+                    alarmIntent.putExtra("number", numTotal)
+                    pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0)
+                    startAlarm(editMinutes.text.toString(), alarmManager)
                     startButton.text = "Stop"
-                    val messageText = editMessage.text.toString()
-                    val phoneText = editPhoneNumber.text.toString()
-                    intent.putExtra("message", messageText)
-                    intent.putExtra("number", phoneText)
-
-                    val alarmIntent = PendingIntent.getBroadcast(this, 0, intent,0)
-                    val time = editMinutes.text.toString().toLong() * 60000
-
-                    alarmManager.setRepeating(
-                        AlarmManager.RTC_WAKEUP,
-                        System.currentTimeMillis(),
-                        time,
-                        alarmIntent
-                    )
-
-                    Log.v("**Main**",
-                        "Alarm started! Message is '$messageText' and number is $phoneText.")
+                    Log.v("**Main**", "Pressed start!")
                 }
             } else {
                 startButton.text = "Start"
-                val alarmIntent = PendingIntent.getBroadcast(this, 0, intent,
-                    PendingIntent.FLAG_CANCEL_CURRENT)
-                alarmManager.cancel(alarmIntent)
-                Log.v("**Main**", "Alarm ended!")
+                alarmManager.cancel(pendingIntent)
+                Log.v("**Main**", "Texts ended!")
             }
+        }
+    }
+
+    private fun startAlarm(minutes: String, alarmManager: AlarmManager) {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            // Need to request SEND_SMS permission
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.SEND_SMS),
+                REQUEST_SMS_SEND_PERMISSION)
+            Log.v("**Main**", "Send SMS permissions")
+        } else {
+            // Has Permissions, Send away!
+            val time = minutes.toLong() * 60000
+            alarmManager.setRepeating(
+                AlarmManager.RTC_WAKEUP,
+                System.currentTimeMillis(),
+                time,
+                pendingIntent
+            )
+            Log.v("**Main**", "Texts started!")
         }
     }
 }
